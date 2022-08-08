bdbfb1e5327c3f6b94f916ecf1e2ab0101f2d38f
Meghana M R <86944692+mrmeghana@users.noreply.github.com>
2022-08-08 10:48:40 +0530

    Create STRING_LENGTH.java

---
+++ STRING_LENGTH.java
@@ -0,0 +1,14 @@
+//finding string length using stringname.length() method
+//code contributed by Meghana M.R.
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class STRING_LENGTH
+{
+    public static void main(String[] args)
+    {
+        String str=new String("Meghana    M.R.");//includes spaces too
+        int a=str.length();
+        System.out.println(a);
+    }
+}
