c1ccf4a4f61ccacf5f223730033afc72d51cd7af
Meghana M R <86944692+mrmeghana@users.noreply.github.com>
2022-08-08 10:11:05 +0530

    Add files via upload

---
+++ strings_declare.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+public class strings_declare
+{
+        public static void main(String[] args)
+        {
+            Scanner sc=new Scanner(System.in);
+            //Method1: Using String method ,passing into it
+            String str=new String("Meghana M.R.");
+            //Method-2: Directly assigning to the variable
+            String str1="Meghana";
+            //Method-3: Asking from the user
+            String str2;
+            str2=sc.nextLine();
+            //Printing all three
+            System.out.println(str);
+            System.out.println(str1);
+            System.out.println(str2);
+            
+
+        }
+}
