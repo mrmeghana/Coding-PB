4b17b4ad9b2165b35c7cd1bf836f7983236e59d2
Meghana M R <86944692+mrmeghana@users.noreply.github.com>
2022-08-08 10:19:59 +0530

    Add files via upload

---
+++ char_to_string.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+public class char_to_string
+{
+    public static void main(String[] args)
+    {
+        char mom[]={'A','M','A','R','A','V','A','T','H','I'};
+        String mummy=new String(mom);
+        System.out.println(mummy);
+        
+    }
+
+}
