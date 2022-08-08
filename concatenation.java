c7bd0b247bbe16404257b267e138cf14af1bcaec
Meghana M R <86944692+mrmeghana@users.noreply.github.com>
2022-08-08 10:44:47 +0530

    Create concatenation.java

---
+++ concatenation.java
@@ -0,0 +1,14 @@
+//Concatenation in java
+//Code contributed by Meghana M.R.
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+public class concatenation
+{
+    public static void main(String[] args)
+    {
+        String str1=new String("Meghana ");
+        String str2=new String("M.R.");
+        System.out.println(str1+str2);// "+" symbol is used for conacatenationin java
+    }
+}
