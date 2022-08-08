7a03ff8dccdef97b4ceffa238d7ab9f9ac9b39ef
Meghana M R <86944692+mrmeghana@users.noreply.github.com>
2022-08-08 14:12:38 +0530

    Create chat_extraction.java

---
+++ chat_extraction.java
@@ -0,0 +1,12 @@
+ public static void main(String[] args)
+    {
+        String str=new String("Meghana");
+        char ch,ch1,ch2,ch3,ch4;
+        ch=str.charAt(0);
+        ch1=str.charAt(1);
+        ch2=str.charAt(2);
+        ch3=str.charAt(3);
+        ch4=str.charAt(5);
+        System.out.println(ch);
+        System.out.println(ch1);
+    }
