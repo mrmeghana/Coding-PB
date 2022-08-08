8c4fb85e6d19ecd3707683e293cf9fc222cca35d
Meghana M R <86944692+mrmeghana@users.noreply.github.com>
2022-08-08 12:41:50 +0530

    Create STRING_JOIN.java

---
+++ STRING_JOIN.java
@@ -0,0 +1,14 @@
+//join() method in strings is mainly to seperate strings or chacaters witha  delimiter
+//the first index occupies a DELIMITER like +,/,-,.,*.....
+//Contributed by Meghana M.R.
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+public class STRING_JOIN
+{
+    public static void main(String[] args)
+    {
+        String str1=String.join("/","08","08","2022");
+        System.out.println(str1);
+    }
+}
